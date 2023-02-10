e67799bf400030cdb086cab2983dcb7a9cf21733
Tanaka I. Kahwai <iankahwai@gmail.com>
2023-02-10 23:13:49 +0200

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Tanaka on 10/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
